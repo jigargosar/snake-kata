5d84c89329f58f1bf78aa8855d4d2b68d1af344b
Jigar Gosar <jigar.gosar@gmail.com>
2020-07-22 18:54:04 +0530

    init

--- src/Main.elm
+++ src/Main.elm
@@ -22,7 +22,7 @@ initial : Mem
 initial =
     { width = 10
     , height = 20
-    , pos = ( 10, 10 )
+    , pos = ( 0, 0 )
     }
 
 
@@ -40,16 +40,25 @@ view _ mem =
         ( x, y ) =
             mem.pos
 
-        sx =
-            toFloat (cellWidth - mem.width * cellWidth + x * cellWidth)
+        cx =
+            toFloat (x * cellWidth) + toFloat cellWidth * 0.5
 
-        sy =
-            toFloat (cellWidth - mem.height * cellWidth + y * cellWidth)
+        cy =
+            toFloat (y * cellWidth) + toFloat cellWidth * 0.5
+
+        gx =
+            toFloat (mem.width * cellWidth) * -0.5
+
+        gy =
+            toFloat (mem.height * cellWidth) * -0.5
     in
-    [ [ square blue cellWidth
-            |> fade 0.8
-            |> move sx sy
-      ]
-        |> group
+    [ group
+        [ group
+            [ square blue cellWidth
+                |> fade 0.8
+                |> move cx cy
+            ]
+            |> move gx gy
+        ]
         |> scale 0.25
     ]
