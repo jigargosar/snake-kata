cb2e91ce1eff948dcaad0af07eb2478bce80be74
Jigar Gosar <jigar.gosar@gmail.com>
2020-07-29 17:21:08 +0530

    Kata 2: without elm playground

--- src/MainV2.elm
+++ src/MainV2.elm
@@ -24,8 +24,28 @@ main =
             , style "gap" "20px"
             , style "padding" "20px"
             ]
-            (viewNCells (w * h))
+            --(viewNCells (w * h))
+            (viewGridCells w h)
         ]
+
+
+viewGridCells w h =
+    let
+        positions =
+            List.range 1 w
+                |> List.concatMap (\x -> List.range 1 h |> List.map (Tuple.pair x))
+
+        viewXYCell ( x, y ) =
+            div
+                [ style "border" "1px dashed"
+                , style "overflow" "hidden"
+                , style "grid-row" (String.fromInt y)
+                , style "grid-column" (String.fromInt x)
+                ]
+                [ text (String.fromInt x ++ ", " ++ String.fromInt y)
+                ]
+    in
+    List.map viewXYCell positions
 
 
 viewNCells n =
