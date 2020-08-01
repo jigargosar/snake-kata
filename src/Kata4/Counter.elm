ef883f56be3824d1d38051ad938afe8dacd86942
Jigar Gosar <jigar.gosar@gmail.com>
2020-08-01 18:06:05 +0530

    KATA 4: extract World.elm

--- src/Kata4/Counter.elm
+++ src/Kata4/Counter.elm
@@ -1,4 +1,4 @@
-module Kata4.Counter exposing (Counter, init)
+module Kata4.Counter exposing (Counter, done, restart, start, step)
 
 
 type alias Counter =
@@ -7,8 +7,23 @@ type alias Counter =
     }
 
 
-init : Int -> Counter
-init limit =
+start : Int -> Counter
+start limit =
     { limit = limit
     , current = 0
     }
+
+
+step : Counter -> Counter
+step counter =
+    { counter | current = counter.current + 1 }
+
+
+done : Counter -> Bool
+done counter =
+    counter.current >= counter.limit
+
+
+restart : Counter -> Counter
+restart counter =
+    { counter | current = 0 }
